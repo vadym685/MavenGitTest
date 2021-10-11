747dd9312955a25c75bb2c87cdfd3ffb5afc674e
vadym685 <Dfl1v4466>
2021-10-11 19:47:19 +0300

    Practice 2. Add new class Hobby

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,7 +6,9 @@ package org.example;
  */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Test Hobby class" );
+        Hobby hobby1 = new Hobby();
+        System.out.println("hobby1: " + hobby1);
     }
 
 }
---
+++ src/main/java/org/example/Hobby.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class Hobby {
+    private String name;
+    private int hoursPerWeek;
+}
