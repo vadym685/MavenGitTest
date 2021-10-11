7aea26da15e92f0ba36854cc98e3059ba2dbfd0b
vadym685 <Dfl1v4466>
2021-10-11 20:18:42 +0300

    Practice 2. update class Hobby

--- src/main/java/org/example/Hobby.java
+++ src/main/java/org/example/Hobby.java
@@ -3,4 +3,28 @@ package org.example;
 public class Hobby {
     private String name;
     private int hoursPerWeek;
+
+    public Hobby() {
+        super();
+    }
+    public Hobby(String name, int hoursPerWeek) {
+        this.name = name;
+        this.hoursPerWeek = hoursPerWeek;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHoursPerWeek() {
+        return hoursPerWeek;
+    }
+
+    public void setHoursPerWeek(int hoursPerWeek) {
+        this.hoursPerWeek = hoursPerWeek;
+    }
 }
